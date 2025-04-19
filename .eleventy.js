9de4058bd8040f6854fa0d484a1f1b9ee74ce6b3
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-04-19 18:54:18 +0200

    Add the upgrade helper plugin to the Eleventy config file

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
+
 module.exports = function (eleventyConfig) {
 	// Creates a global variable for the current __dirname to make including and
 	// working with files in the pattern library a little easier
@@ -6,6 +8,9 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/fonts");
 	eleventyConfig.addPassthroughCopy("src/images");
 	eleventyConfig.addPassthroughCopy("src/js");
+
+	// If you have other `addPlugin` calls, it’s important that UpgradeHelper is added last.
+	eleventyConfig.addPlugin(UpgradeHelper);
 
 	return {
 		markdownTemplateEngine: "njk",
