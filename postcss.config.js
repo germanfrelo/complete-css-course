624124ed6fe0c401e1b25cb3828548d192a14ab2
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-06-30 13:40:26 +0200

    Clarify a PostCSS plugin order

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 export default {
 	plugins: {
-		"postcss-import-ext-glob": {},
+		"postcss-import-ext-glob": {}, // This plugin must be placed before the "postcss-import" plugin
 		"postcss-import": {},
 		"tailwindcss/nesting": "postcss-nesting",
 		"tailwindcss": {},
