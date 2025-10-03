b2999ff4ac6a04e4b2c4e44a44cd28e74a81aa5e
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-10-03 19:03:59 +0200

    chore(eleventy): Enable hot reloading for CSS
    Update the Eleventy configuration to watch for changes in the `src/css`
    and `dist/css` directories. This enables the dev server to
    automatically hot-reload CSS changes without a full page refresh.

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,12 @@ export default function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/fonts");
 	eleventyConfig.addPassthroughCopy("src/images");
 	eleventyConfig.addPassthroughCopy("src/js");
+	// Tell Eleventy to watch your source CSS files for changes.
+	eleventyConfig.addWatchTarget("src/css");
+	// Tell the dev server to watch the compiled CSS for hot-reloading.
+	eleventyConfig.setServerOptions({
+		watch: ["dist/css/**/*.css"],
+	});
 
 	return {
 		markdownTemplateEngine: "njk",
