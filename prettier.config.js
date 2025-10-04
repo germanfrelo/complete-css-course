bcb88197acd76ea1c121892fcfc44b22081f5f70
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-10-04 16:03:55 +0200

    Add prettier-plugin-jinja-template to Prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -5,6 +5,7 @@
  * @type {import("prettier").Config}
  */
 const config = {
+	plugins: ["prettier-plugin-jinja-template"],
 	quoteProps: "consistent",
 	overrides: [
 		{
@@ -23,6 +24,12 @@ const config = {
 			files: ["*.jsonc"],
 			options: {
 				trailingComma: "none",
+			},
+		},
+		{
+			files: ["*.njk"],
+			options: {
+				parser: "jinja-template",
 			},
 		},
 	],
